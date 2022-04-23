ed6b1d030b1db58002f4bd3f606560e12a4b4435
chohahyun99 <whgkgus1999@gmail.com>
2022-04-23 18:39:03 +0900

    signUp.jsp, mypage_infosetting.jsp 비밀번호 이미지 오류 수정

--- src/main/webapp/resources/js/mypage/jsMypageInfoSetting.js
+++ src/main/webapp/resources/js/mypage/jsMypageInfoSetting.js
@@ -53,22 +53,22 @@
               error[0].style.display = "block";
               
               pwMsg.style.display = "block";
-              pwImg1.src = "${pageContext.request.contextPath}/resources/pngs/m_icon_not_use.png";
+              pwImg1.src = "/springframework-mini-project/resources/pngs/m_icon_not_use.png";
           } else {
               error[0].style.display = "none";
               pwMsg.innerHTML = "안전";
               pwMsg.style.display = "block";
               pwMsg.style.color = "#03c75a";
-              pwImg1.src = "${pageContext.request.contextPath}/resources/pngs/m_icon_safe.png";
+              pwImg1.src = "/springframework-mini-project/resources/pngs/m_icon_safe.png";
           }
       }
     
       function comparePw() {
           if(pw2.value === pw1.value && pw2.value != "") {
-              pwImg2.src = "${pageContext.request.contextPath}/resources/pngs/m_icon_check_enable.png";
+              pwImg2.src = "/springframework-mini-project/resources/pngs/m_icon_check_enable.png";
               error[1].style.display = "none";
           } else if(pw2.value !== pw1.value) {
-              pwImg2.src = "${pageContext.request.contextPath}/resources/pngs/m_icon_check_disable.png";
+              pwImg2.src = "/springframework-mini-project/resources/pngs/m_icon_check_disable.png";
               error[1].innerHTML = "비밀번호가 일치하지 않습니다.";
               error[1].style.display = "block";
           } 
