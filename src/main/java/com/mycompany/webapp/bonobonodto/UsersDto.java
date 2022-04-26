ea8adbd4070faf54b4df3cc782f51e2714f395b5
chohahyun99 <whgkgus1999@gmail.com>
2022-04-26 13:24:07 +0900

    InteriorDto, InteriorDetailDto,UsersDto

---
+++ src/main/java/com/mycompany/webapp/bonobonodto/UsersDto.java
@@ -0,0 +1,16 @@
+package com.mycompany.webapp.bonobonodto;
+
+import lombok.Data;
+
+@Data
+public class UsersDto {
+	private String email; //이메일, 로그인할때 id로 사용
+	private String password; //비밀번호
+	private String name; //이름
+	private String phone; //대표번호
+	private String postcode; //우편번호
+	private String address; //주소
+	private String addressDetail; //상세주소
+	private String crn; //사업자등록번호
+	
+}
