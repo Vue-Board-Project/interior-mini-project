8746b9461b594eb921db2d74b1a196e13d3b56f7
chohahyun99 <whgkgus1999@gmail.com>
2022-04-26 16:45:35 +0900

    users.xml 생성

--- src/main/java/com/mycompany/webapp/dao/mybatis/UsersDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/UsersDao.java
@@ -2,9 +2,27 @@ package com.mycompany.webapp.dao.mybatis;
 
 import javax.annotation.Resource;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.mycompany.webapp.dto.UsersDto;
 
 @Repository
 public class UsersDao {
-
+	
+	//?
+	@Resource
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	//users insert
+	public int insert(UsersDto users) {
+		int rows = sqlSessionTemplate.insert("com.mycompany.webapp.dao.mybatis.UsersDao.insert",users);
+			return rows;
+	}
+	
+	//users email select
+	public UsersDto selectByEmail(String email) {
+		UsersDto usersDto = sqlSessionTemplate.selectOne("com.mycompany.webapp.dao.mybatis.UsersDao.selectByEmail",email);
+		return usersDto;
+	}
 }
