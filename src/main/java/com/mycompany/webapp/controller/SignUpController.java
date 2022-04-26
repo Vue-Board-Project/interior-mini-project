fad92a5e9a18c7a29a5420539375be8fc4cce013
chohahyun99 <whgkgus1999@gmail.com>
2022-04-26 17:08:59 +0900

    usersService 생성, 회원가입 Controller 수정, security 해줘야함

--- src/main/java/com/mycompany/webapp/controller/SignUpController.java
+++ src/main/java/com/mycompany/webapp/controller/SignUpController.java
@@ -1,10 +1,16 @@
 package com.mycompany.webapp.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.webapp.dto.UsersDto;
+import com.mycompany.webapp.service.UsersService;
+import com.mycompany.webapp.service.UsersService.JoinResult;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -30,5 +36,26 @@ public class SignUpController {
 	public String join(HttpServletRequest request) {
 		log.info(request);
 		return "home";
+	}
+	
+	@Resource
+	private UsersService usersService;
+	
+	//회원가입
+	@RequestMapping("/join")
+	public String join(UsersDto users, Model model) {
+		log.info("join running");
+		
+		//회원 가입 처리
+		JoinResult jr = usersService.join(users);
+		if(jr == JoinResult.SUCCESS) {
+			return "redirect:/";
+		} else if(jr ==JoinResult.DUPLICATED) {
+			model.addAttribute("error", "중복된 아이디가 있습니다.");
+			return "signUp";
+		} else {
+			model.addAttribute("error", "회원 가입이 실패되었습니다. 다시 시도해 주세요.");
+			return "signUp";
+		}
 	}
 }
--- src/main/java/com/mycompany/webapp/service/UsersService.java
+++ src/main/java/com/mycompany/webapp/service/UsersService.java
@@ -2,8 +2,11 @@ package com.mycompany.webapp.service;
 
 import javax.annotation.Resource;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.mycompany.webapp.dao.mybatis.UsersDao;
 import com.mycompany.webapp.dto.UsersDto;
 
 import lombok.extern.log4j.Log4j2;
@@ -11,5 +14,28 @@ import lombok.extern.log4j.Log4j2;
 @Service
 @Log4j2
 public class UsersService {
-
+	public enum JoinResult{
+		SUCCESS, FAIL, DUPLICATED
+	}
+	
+	public enum LoginResult{
+		SUCCESS, FAIL_MID, FAIL_MPASSWORD, FAIL
+	}
+	
+	//?
+	@Resource
+	private UsersDao usersDao;
+	
+	//회원가입
+	public JoinResult join(UsersDto users) {
+		UsersDto dbUsers = usersDao.selectByEmail(users.getEmail());
+		if(dbUsers == null) {
+			PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+			users.setPassword(passwordEncoder.encode(users.getPassword()));
+			int result = usersDao.insert(users);
+			return JoinResult.SUCCESS;
+		} else {
+			return JoinResult.DUPLICATED;
+		}
+	}
 }
