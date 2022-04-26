f8566d0815f925ee068cb432b716b65d3abdf1ea
PiaHan <hbh0604@naver.com>
2022-04-26 13:26:35 +0900

    DTO 추가 (interior_progress,interior progress file, reference model, solution) DB 관련

---
+++ src/main/java/com/mycompany/webapp/dto/MyPageSolutionDto.java
@@ -0,0 +1,10 @@
+package com.mycompany.webapp.dto;
+
+import lombok.Data;
+
+@Data
+public class MyPageSolutionDto {
+	private int sno; // 솔루션 시퀀스
+	private String scontent;	//솔루션 내용
+	//private int consult_no;	접수번호(FK)
+}
