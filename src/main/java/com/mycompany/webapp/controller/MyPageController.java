b75e4c67c5fe15ade61888018562bc46e688b55e
PiaHan <hbh0604@naver.com>
2022-04-27 21:15:20 +0900

    헤더에 taglib 추가, mypage review란 MVC 연결 중

--- src/main/java/com/mycompany/webapp/controller/MyPageController.java
+++ src/main/java/com/mycompany/webapp/controller/MyPageController.java
@@ -1,63 +1,95 @@
 package com.mycompany.webapp.controller;
 
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mycompany.webapp.dto.mypage.ReviewDto;
+import com.mycompany.webapp.service.MypageService;
 
 import lombok.extern.log4j.Log4j2;
 
 @Controller
+@RequestMapping("/mypage")
 @Log4j2
 public class MyPageController {
 	
-	@RequestMapping("/mypage/mypage_counseling")
-	public String mypageCounseling() {
-		log.info("구매내역");
-		
-		return "mypage/mypage_counseling";//view 이름만 전달
-	}
 	
-	@RequestMapping("/mypage/myinfo_counsel_popup")
+	 @RequestMapping("/mypage_counseling") 
+	 public String mypageCounseling(){ 
+		 log.info("구매내역");
+		 return "mypage/mypage_counseling";
+	 }
+	 
+	 @Resource private MypageService mypageService;
+	 
+	/*
+	 * @Resource private MypageService mypageService;
+	 * 
+	 * @GetMapping("/mypage/mypage_counseling") public String
+	 * mypageCounseling(String email) { log.info("구매내역");
+	 * 
+	 * return "mypage/mypage_counseling";//view 이름만 전달 }
+	 */
+	
+	@RequestMapping("/myinfo_counsel_popup")
 	public String mypagePopup() {
 		
 		return "mypage/myinfo_counsel_popup";
 	}
 	
-	@RequestMapping("/mypage/mypage_infosetting")
+	@RequestMapping("/mypage_infosetting")
 	public String mypageInfosetting() {
 		
 		return "mypage/mypage_infosetting";
 	}
 
-	@RequestMapping("/mypage/mypage_orderlist")
+	@RequestMapping("/mypage_orderlist")
 	public String mypageOrderList() {
 		
 		return "mypage/mypage_orderlist";
 	}
 	
-	@RequestMapping("/mypage/mypage_interior_report")
+	@RequestMapping("/mypage_interior_report")
 	public String mypageInteriorReport() {
 		
 		return "/mypage/mypage_interior_report";
 	}
 
 	
-	@RequestMapping("/mypage/mypage_interior_progress")
+	@RequestMapping("/mypage_interior_progress")
 	public String mypageInteriorProgress() {
 		return "/mypage/mypage_interior_progress";
 	}
-	
-	@RequestMapping("/mypage/mypage_review")
-	public String mypageReview() {
+	@RequestMapping("/mypageReview")
+	public String mypageReviewPage() {
+		
 		return "/mypage/mypage_review";
 	}
 	
-	@RequestMapping("/mypage/popupSample")
+	//@PostMapping("/mypage/mypage_review")
+	//@RequestMapping(value = "/mypage/mypage_review", method = RequestMethod.POST)
+	@PostMapping("/mypageReview")
+	public String mypageReview(ReviewDto review){
+		log.info("작동확인");
+		log.info(review.getReviewTitle());
+		log.info(review.getReviewContent());
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/popupSample")
 	public String mypagePopupSample() {
 		return "/mypage/mypage_popup_practice";
 	}
 	
 	
-	@RequestMapping("/mypage/device_AS")
+	@RequestMapping("/device_AS")
 	public String mypageDeviceAS() {
 		return "/mypage/mypage_device_AS";
 	}
