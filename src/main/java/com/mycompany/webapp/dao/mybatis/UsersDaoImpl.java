ced8d4931324f9542faf8c258573f24fc8ccba71
chohahyun99 <whgkgus1999@gmail.com>
2022-05-03 11:54:12 +0900

    트랜잭션 추가

--- src/main/java/com/mycompany/webapp/dao/mybatis/UsersDaoImpl.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/UsersDaoImpl.java
@@ -1,10 +1,8 @@
 package com.mycompany.webapp.dao.mybatis;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.mycompany.webapp.dto.UsersDto;
 
@@ -25,7 +23,6 @@ public class UsersDaoImpl implements UsersDao {
 	}
 
 	@Override
-	@Transactional
 	public int updatePW(UsersDto users) throws Exception {
 		return sqlSession.update("com.mycompany.webapp.dao.mybatis.UsersDao.updatePW", users);
 	}
